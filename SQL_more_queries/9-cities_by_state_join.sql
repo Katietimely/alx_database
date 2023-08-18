9557a0ed07c6e6bd66f8884862e22c570ee3ab36
Irabor Kate Chika <iraborkate29@gmail.com>
2023-08-18 12:00:21 +0100

    JOIN

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- listing all cities
 SELECT cities.id, cities.name, states.name
 FROM cities
-join states ON cities.state_id = states.id
+JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
